86966079aa7055e98857c6f6f6532904d963b6cc
semicolon9 <santoshvishwakarma.web@gmail.com>
2023-01-31 22:13:38 +0530

    CommonHelper

---
+++ FirstCoreProject.CommonHelper/PaymentStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCoreProject.CommonHelper
+{
+    public static class PaymentStatus
+    {
+
+        public const string StatusPending = "Pending";
+        public const string StatusRefund = "Refunded";
+
+        public const string StatusApproved = "Approved";
+
+        public const string StatusRejected = "Rejected";
+
+        public const string StatusPaymentDelayed = "Delay";
+
+
+    }
+}
